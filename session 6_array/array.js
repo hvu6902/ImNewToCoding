58780ae2f07199e7122a686acbce9972ea69254a
hvu6902 <hoangvupantc@gmail.com>
2020-11-13 21:02:50 +0700

    test

---
+++ session 6_array/array.js
@@ -0,0 +1,59 @@
+// let h = [ 150, 155, 170, 145, 180 ];
+// let avr = 0;
+// let sum = 0;
+
+// for (let index = 0; index < h.length; index++) {
+
+//     console.log(h[index]);
+
+// sum = sum + h[index];
+// avr = sum / (index +1);
+// }
+// console.log(`Trung bình: ${avr}`);
+    
+// 4
+// let max = h[0];
+// for (let i = 0; h < h.length; i++) {
+//   if (h[i] > max) {
+//       max = h
+//     }
+// }
+// console.log(`Cao nhất: ${max}`);
+// for (let index = 0; index < h.length; index++) {
+//     for (let j  = j+1; j  < h.length; j ++){
+//         let temp  = h[index]
+//         h[i] = h[j];
+//         h[j] = temp
+//         }
+//         console.log(`Tăng dần`);
+//     }
+// Có bao nhiêu người thấp hơn 1m6
+// let count = 0;
+// for (let i = 0; i < h.length; i++) {
+//     if (h < 160) {
+//         console.log(h);
+//         a.push (h);
+//     }
+    
+// }
+
+// let a = [];
+// for (let i = 0; i < h.length; i++) {
+//     if (h < 160){
+//         a.push(h);
+//     }
+// }
+// if (a.length > 0){
+
+// }
+
+// nhập 1 số >= 8, nhỏ hơn thì nhập lại
+let n = Number(prompt("Nhập 1 số"));
+
+while (n < 8) {
+    n = Number(prompt("Nhập lại số >= 8"));
+}
+console.log(n);
+
+
+    
